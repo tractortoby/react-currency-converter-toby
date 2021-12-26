38cb34149c9a62e181e4273c3038d8923423d495
tractortoby <tobychung1993@gmail.com>
2021-12-26 22:16:16 +0800

    add Exchangerates API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import './App.css';
-import CurrencyRow from './CurrencyRow';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+const ACCESS_KEY = "d550e80a38cb3394859f2f09daf15cb8";
+const BASE_URL = `http://api.exchangeratesapi.io/v1/latest?access_key=${ACCESS_KEY}`;
 
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <>
       <h1>Convert</h1>
       <CurrencyRow />
-      <div className='equals'>=</div>
+      <div className="equals">=</div>
       <CurrencyRow />
     </>
   );
