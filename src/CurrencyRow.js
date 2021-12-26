35bbc036cdefe91064e813a443108e392bea6650
tractortoby <tobychung1993@gmail.com>
2021-12-26 21:42:07 +0800

    finish basic content

---
+++ src/CurrencyRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CurrencyRow() {
+  return (
+    <div>
+      {/* <label htmlFor="">Amount</label> */}
+      <input type="number" name="number" id="number" className="input" />
+      <select name="" id="">
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  );
+}
