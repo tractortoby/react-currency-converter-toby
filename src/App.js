679bafbb82310cdcb81f42b5c52d599baabdf29e
tractortoby <tobychung1993@gmail.com>
2021-12-27 16:00:04 +0800

    Add Prettier and ESLint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import CurrencyRow from "./CurrencyRow";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow';
 
-const ACCESS_KEY = "d550e80a38cb3394859f2f09daf15cb8";
+const ACCESS_KEY = 'd550e80a38cb3394859f2f09daf15cb8';
 const BASE_URL = `http://api.exchangeratesapi.io/v1/latest?access_key=${ACCESS_KEY}`;
 
 function App() {
   useEffect(() => {
     fetch(BASE_URL)
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <>
